93db6d5d0ca7731488919140e79a6d4843dbf51a
Aviv Gelfand <63909805+AvivGelfand@users.noreply.github.com>
2023-11-07 14:14:49 +0200

    Add files via upload

---
+++ import json.py
@@ -0,0 +1,21 @@
+import json
+import re
+
+def get_imports_from_notebook(notebook_path):
+    with open(notebook_path, 'r') as f:
+        data = json.load(f)
+
+    imports = set()
+    for cell in data['cells']:
+        if cell['cell_type'] == 'code':
+            for line in cell['source']:
+                match = re.match(r'import (\S+)|from (\S+) import', line)
+                if match:
+                    imports.add(match.group(1) or match.group(2))
+
+    return list(imports)
+
+# Use the function
+notebook_path = '1-poc-watch-nn-wheights\main.ipynb'  # replace with your notebook path
+imports = get_imports_from_notebook(notebook_path)
+print(imports)
